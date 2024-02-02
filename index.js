204c21b05caebaf2d667aeaddf5aa51281af08c0
ismailajizou <ismailajizoumo@gmail.com>
2024-02-02 11:18:44 +0100

    some changes

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 console.log("Hello World from index.js!")
 console.log("First fearture added")
+console.log("Second feature added")
---
+++ math.js
@@ -0,0 +1,4 @@
+console.log("Hello World from math.js!")
+console.log("First fearture added")
+console.log("First fearture added")
+console.log("First fearture added")
