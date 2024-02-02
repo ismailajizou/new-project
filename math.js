a8d4d2605c0b29aae9d562bdc9351d8f938c141d
ismailajizou <ismailajizoumo@gmail.com>
2024-02-02 11:40:45 +0100

    added a new buton

--- math.js
+++ math.js
@@ -3,3 +3,6 @@ console.log("First fearture added")
 console.log("First fearture added")
 console.log("First fearture added")
 console.log("First fearture added")
+
+
+console.log("That's a feature implemented in  button branch")
